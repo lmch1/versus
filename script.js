024a20730bd7c720d9e21df9933cb14687415aa7
Lekso <muchiashvili.2001@gmail.com>
2025-05-23 15:30:50 +0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const countDownDate = new Date("July 9, 2025 13:00:00").getTime();
+
+    const x = setInterval(function() {
+        const now = new Date().getTime();
+        const distance = countDownDate - now;
+
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        document.getElementById("days").innerHTML = days;
+        document.getElementById("hours").innerHTML = hours;
+        document.getElementById("minutes").innerHTML = minutes;
+        document.getElementById("seconds").innerHTML = seconds;
+
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("countdown").innerHTML = "IT'S TIMEEEEEE!";
+        }
+    }, 1000);
+});
